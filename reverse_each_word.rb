2f057e3595a8be2a7b4ddaa199e46bee198cca6e
James Scalise <jamescscalise@gmail.com>
2019-08-04 19:30:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,9 @@ def reverse_each_word(string)
   return array.join(" ")
 end
 
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.each {|word| puts word.reverse!}
+
+  return array.join(" ")
+end
